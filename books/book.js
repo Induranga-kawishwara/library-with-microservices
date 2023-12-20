5a4ac9da80803a4b2744ca301af9dad6777368c9
Induranga Kawishwara <induranga.20200688@iit.ac.lk>
2023-12-20 23:51:44 +0530

    update

--- books/book.js
+++ books/book.js
@@ -1,13 +1,20 @@
-const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
 
-const app = express();
-mongoose.connect(process.env.MONGODB).then(() => console.log("connected"));
-
-app.get("/", (req, res) => {
-  res.send("This is our main endpoint !!");
-});
-app.listen(4545, () => {
-  console.log("Up and running -- This is our Book server");
+mongoose.model("Book", {
+  title: {
+    type: String,
+    require: true,
+  },
+  author: {
+    type: String,
+    require: true,
+  },
+  numberPage: {
+    type: String,
+    require: false,
+  },
+  publisher: {
+    type: String,
+    require: false,
+  },
 });
---
+++ books/books.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+mongoose.connect(process.env.MONGODB).then(() => console.log("connected"));
+
+app.get("/", (req, res) => {
+  res.send("This is our main endpoint !!");
+});
+
+app.post("/book", (req, res) => {
+  res.send("testing");
+});
+
+app.listen(4545, () => {
+  console.log("Up and running -- This is our Book server");
+});
