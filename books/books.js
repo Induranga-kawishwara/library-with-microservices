3debe9f2e8fabfd88a1960add50aa2de5371b552
Induranga Kawishwara <induranga.20200688@iit.ac.lk>
2023-12-21 11:00:50 +0530

    update

--- books/books.js
+++ books/books.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
 mongoose.connect(process.env.MONGODB).then(() => console.log("connected"));
 
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("This is our main endpoint !!");
 });
 
 app.post("/book", (req, res) => {
+  console.log(req.body);
   res.send("testing");
 });
 
